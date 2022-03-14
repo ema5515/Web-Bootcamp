357a7b3b7aee1c255f7b91959465b1205a8f697f
Emanuele D'Agata <ema.dagata@gmail.com>
2022-03-14 20:46:16 +0100

    20220314

---
+++ JavaScript_1/js/index.js
@@ -0,0 +1,5 @@
+document.querySelector(".item a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "#ffff00";
+
+document.querySelector("h1").classList.toggle("bigger");
