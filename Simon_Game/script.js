e62228d04e938bfaf65b239cce618161db79526a
Emanuele D'Agata <ema.dagata@gmail.com>
2022-04-07 14:57:01 +0200

    20220406

--- Simon_Game/script.js
+++ Simon_Game/script.js
@@ -2,60 +2,49 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var colour;
-var level = -1;
+var level = 0;
+var i;
+var started = false;
+var lastClickedButton;
 
-$("html").keypress(function(){
-    if(($("#level-title").text()) == "Press A Key to Start"){
+//inizio del gioco
+$(document).keypress(function(){
+    if(!started){
+        started = true;
         nextSequence();
     }else{
         alert("game already started! reload to restart...");
     }
 });
 
-
-
+//funzione base del gioco
 function nextSequence(){
-    level += 1;
+    level++;
     $("#level-title").text("level " + level);
+    i = 0;
 
     //scegle un colore e lo inserisce a fine gamepattern
-    var randomNumber = Math.floor((Math.random())*4);
+    var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber]; 
     gamePattern.push(randomChosenColour);
     console.log(gamePattern);
 
-    playGamePattern();
+    setTimeout(function(){
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    }, 500);
+    
+    
 };
 
-function playGamePattern(){
-
-    
-    for(i=0; i < gamePattern.length; i++){
-        var delayTime = (i+1)*700;
-
-        setTimeout(function(){
-            animation(gamePattern[i]);
-            console.log("in" + 1);
-            console.log("in" + gamePattern[i]);
-        }, delayTime);
-        
-    
-        console.log("out" + 1);
-        console.log("out gp " + gamePattern[i]);
-    };
+function restart(){
+    $("#level-title").text("level " + level + "\n\n GAME OVER!\n");
+    $(".container").addClass("display-no");
+    $(".restart-button").removeClass("display-no");
+    $("#level-title").fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150);
 }
 
-function userGamePattern(){
 
-};
-
-
-
-function handler(a) {
-    userClickedPattern.push(a);
-};
-
-
+//animazione e suono dei pulsanti
 function animation(a){
     $("." + a).addClass("pressed");
     setTimeout(function(){
@@ -66,8 +55,23 @@ function animation(a){
 
 $(".btn").click(function(){
     colour = $(this).attr("id");
-    handler(colour);
     animation(colour);
     var audio = new Audio("sounds/" + colour + ".mp3");
     audio.play();
+
+    if(colour===gamePattern[i]){
+        console.log("perfect");
+        console.log("before " + i)
+        i++;
+        console.log("after " + i)   
+    } else{
+        restart();
+    }
+        if(i===gamePattern.length){
+            nextSequence();
+        }
 });
+
+$(".restart-button").click(function(){
+    window.location.reload();
+})
