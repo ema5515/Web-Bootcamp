cd125ad36ec1d5f06d6f23f8c3258d26536e257d
Emanuele D'Agata <ema.dagata@gmail.com>
2022-04-12 22:02:08 +0200

    20220412

--- Express_1/my-express-server/server.js
+++ Express_1/my-express-server/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.get('/', function(req, res){
+    console.log(req);
+    res.send("Hello");
+});
+
+app.get('/contact', function(req, res){
+    console.log(req);
+    res.send("../../CSex.htjhgfjgfml");
+} )
+
 app.listen(8080, function(){
     console.log("my first server start on port 8080")
 });
